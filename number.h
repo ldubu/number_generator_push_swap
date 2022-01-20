8b52538066018f3d070dd818267a6f29b04a76bc
Laura DUBUCHE <ldubuche@e2r9p7.clusters.42paris.fr>
2022-01-20 10:51:24 +0100

    probleme nombre negatif

---
+++ number.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   number.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldubuche <laura.dubuche@gmail.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/20 09:32:21 by ldubuche          #+#    #+#             */
+/*   Updated: 2022/01/20 10:28:49 by ldubuche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef NUMBER_H
+# define NUMBER_H
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int	ft_atoi(const char *str);
+int is_in_tab(int tab[], int nbr, int i);
+
+#endif
